9afafad53c3eea83d18b87ee81b9d8d7a00ea30c
zhangshixin <zhangshixin@weibai.co>
2017-08-23 17:24:06 +0800

    daily update

---
+++ c/leetcode4.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size) {
+  int l = 0, r = 0;
+  while (l < nums1 && r < nums2){
+    
+  }
+}
+int main(int argc, const char *args[]){
+  
+}
---
+++ kotlin/test.kt
@@ -0,0 +1,5 @@
+fun main(args: Array<String>){
+  val d : Double = 111111111111.0
+  print("%.0f".format(d))
+  println()
+}
